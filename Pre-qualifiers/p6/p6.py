43ade062e4a7f13693cf230e167973c0583fa9f5
Luke Chen <lukechn99@gmail.com>
2020-12-05 03:00:19 -0600

    finished p6

--- Pre-qualifiers/p6/p6.py
+++ Pre-qualifiers/p6/p6.py
@@ -7,6 +7,8 @@
 # lengths of the integers in the list
 ###########################################################
 
+import unittest
+
 # takes a string, and the indices to check between
 def palindrome_helper(string, base, ceiling):
     if ceiling - base <= 1:
@@ -22,17 +24,20 @@ def solve(string, palindrome_lengths):
     count = 0
     for length in palindrome_lengths:
         # skip if palin query is longer than string
-        if length >= len(string):
+        if length > len(string):
             continue
         base = 0
         ceiling = length - 1
         while ceiling < len(string):
             if palindrome_helper(string, base, ceiling):
-                print("found %s" %(string[base:ceiling]))
                 count += 1
             base += 1
             ceiling += 1
     return count
+
+# solve in one pass
+def solve1(string, palindrome_lengths):
+    pass
 
 def main():
     # taking input
@@ -42,4 +47,16 @@ def main():
     palindrome_lengths = list(map(int, input().split()))
     print(solve(string, palindrome_lengths))
 
-print(solve("abccbaabccba", [1, 3, 5]))
+class Test(unittest.TestCase):
+    def test_tcs1(self):
+        string = "xyxzyxyz"
+        lengths = [1, 3, 5]
+        self.assertEqual(solve(string, lengths), 11)
+    def test_tcs2(self):
+        string = "abccbaabccba"
+        lengths = [4, 6, 12]
+        self.assertEqual(solve(string, lengths), 7)
+
+if __name__=="__main__": 
+    main()
+    # unittest.main()
